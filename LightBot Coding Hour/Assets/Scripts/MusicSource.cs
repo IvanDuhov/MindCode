235c00227decca7f7654fbfcf08f9b2bfb195a70
Ivan Duhov <duhovgames@gmail.com>
2018-03-21 22:42:28 +0200

    Setup fix

---
+++ LightBot Coding Hour/Assets/Scripts/MusicSource.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicSource : MonoBehaviour 
+{
+	void Awake() 
+	{
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
---
+++ LightBot Coding Hour/Assets/Scripts/Tile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour 
+{
+	public float xpos { get; set; }
+	public float zpos { get; set; }
+	public float height { get; set; }
+	public string colour = "Grey";
+	public MeshRenderer colorGrid;
+
+	void Start()
+	{
+		xpos = transform.localPosition.x;
+		zpos = transform.localPosition.z;
+		height = transform.localPosition.y;
+	}
+}
